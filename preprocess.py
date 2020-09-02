9e76bc65684126afa3d0aa0ae5c14b7da4111387
Xskullibur <alsonproj2222@gmail.com>
2020-09-02 18:18:56 +0800

    JSON to CSV completed

--- preprocess.py
+++ preprocess.py
@@ -5,9 +5,28 @@ JSON_FILE = './input/WLASL_v0.3.json'
 OUTPUT_FILE = './output/WLASL.csv'
 
 
-def read_JSON():
-    pd.read_json(JSON_FILE)
+def jsonToCSV():
+    # Read JSON File
+    data = pd.read_json(JSON_FILE, 'values')
+
+    # Convert instance (object) into DataFrame
+    instances = pd.DataFrame.from_records(data['instances'])
+    data = data.join(instances)
+
+    # Drop Old Instances
+    data = data.drop('instances', 1)
+
+    # Reformat DataFrame
+    data = data.melt(id_vars=['gloss'], value_name='instance')
+
+    # Drop Missing values
+    data = data.dropna()
+    data = data.drop('variable', 1)
+
+    data = data.join(pd.DataFrame.from_records(data['instance']))
+    data = data.loc[:, ['gloss', 'bbox', 'video_id', 'frame_end', 'frame_start']]
+    data.to_csv(OUTPUT_FILE, index=False)
 
 
 if __name__ == '__main__':
-    read_JSON()
+    jsonToCSV()
