85739b4384f8626c2175c84a35bf6bd257a5e542
Xskullibur <alsonproj2222@gmail.com>
2020-09-04 22:33:39 +0800

    Image modification

--- pose_estimation.py
+++ pose_estimation.py
@@ -5,7 +5,7 @@ from sys import platform
 import argparse
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-IMAGE_PATH = DIR_PATH + '/input/images/temp'
+IMAGE_PATH = DIR_PATH + '/input/images/temp/'
 
 
 def estimate_pose(image):
@@ -25,7 +25,7 @@ def estimate_pose(image):
             # OpenPose/python module from there. This will install OpenPose and the python library at your desired
             # installation path. Ensure that this is in your python path in order to use it. sys.path.append(
             # '/usr/local/python')
-        from openpose import pyopenpose as op
+            from openpose import pyopenpose as op
 
     except ImportError as e:
         print(
@@ -64,9 +64,10 @@ def estimate_pose(image):
     # oppython = op.OpenposePython()
 
     # Starting OpenPose
-    opWrapper = op.WrapperPython(3)
+    opWrapper = op.WrapperPython()
     opWrapper.configure(params)
     opWrapper.execute()
+    # opWrapper.start()
 
     # Get information
     datum = op.Datum()
@@ -77,82 +78,9 @@ def estimate_pose(image):
     # Display Image
     print("Body keypoints: \n" + str(datum.handKeypoints))
 
-# # From Python
-# # It requires OpenCV installed for Python
-# import sys
-# import cv2
-# import os
-# from sys import platform
-# import argparse
-#
-# from statsmodels.genmod.tests.results.res_R_var_weight import dir_path
-#
-# try:
-#     # Import Openpose (Windows/Ubuntu/OSX)
-#     dir_path = os.path.dirname(os.path.realpath(__file__))
-#     try:
-#         # Windows Import
-#         if platform == "win32":
-#             # Change these variables to point to the correct folder (Release/x64 etc.)
-#             sys.path.append(dir_path + '/pose_estimation/Release')
-#             os.environ['PATH'] = os.environ['PATH'] + ';' + dir_path + '/pose_estimation/Release;' + dir_path + '/pose_estimation/bin;'
-#             import pyopenpose as op
-#         else:
-#             # Change these variables to point to the correct folder (Release/x64 etc.)
-#             sys.path.append('../../python');
-#             # If you run `make install` (default path is `/usr/local/python` for Ubuntu), you can also access the
-#             # OpenPose/python module from there. This will install OpenPose and the python library at your desired
-#             # installation path. Ensure that this is in your python path in order to use it. sys.path.append(
-#             # '/usr/local/python')
-#             from openpose import pyopenpose as op
-#     except ImportError as e:
-#         print(
-#             'Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python '
-#             'script in the right folder?')
-#         raise e
-#
-#     # Flags
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("--image_path", default="../../../examples/media/COCO_val2014_000000000192.jpg",
-#                         help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
-#     args = parser.parse_known_args()
-#
-#     # Custom Params (refer to include/openpose/flags.hpp for more parameters)
-#     params = dict()
-#     params["model_folder"] = dir_path + "/pose_estimation/bin/models/"
-#
-#     # Add others in path?
-#     for i in range(0, len(args[1])):
-#         curr_item = args[1][i]
-#         if i != len(args[1]) - 1:
-#             next_item = args[1][i + 1]
-#         else:
-#             next_item = "1"
-#         if "--" in curr_item and "--" in next_item:
-#             key = curr_item.replace('-', '')
-#             if key not in params:  params[key] = "1"
-#         elif "--" in curr_item and "--" not in next_item:
-#             key = curr_item.replace('-', '')
-#             if key not in params: params[key] = next_item
-#
-#     # Construct it from system arguments
-#     # op.init_argv(args[1])
-#     # oppython = op.OpenposePython()
-#
-#     # Starting OpenPose
-#     opWrapper = op.WrapperPython(3)
-#     opWrapper.configure(params)
-#     opWrapper.execute()
-#
-#     # Get information
-#     datum = op.Datum()
-#     imageToProcess = cv2.imread(args[0].image_path)
-#     datum.cvInputData = imageToProcess
-#     opWrapper.emplaceAndPop([datum])
-#
-#     # Display Image
-#     print("Body keypoints: \n" + str(datum.handKeypoints))
-#
-# except Exception as e:
-#     print(e)
-#     sys.exit(-1)
+    # cv2.imshow("OpenPose 1.6.0 - Tutorial Python API", datum.cvOutputData)
+    # cv2.waitKey()
+
+
+if __name__ == '__main__':
+    estimate_pose('test1.jpg')
